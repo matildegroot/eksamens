590aac657a577c9ac892dcfe6762263c8b7e7404
Magr <magr0006@stud.kea.dk>
2025-01-06 12:12:44 +0100

    gem lige

--- JS/site.js
+++ JS/site.js
@@ -1,17 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => { // Sørger for at koden kører efter siden er loadet
-    const text = "Om mig"; // Teksten der skal skrives
+// OM MIG TYPEWRITER EFFEKT
+
+document.addEventListener("DOMContentLoaded", () => { 
+    const text = "Om mig"; 
     let index = 0;
   
     function typeWriter() {
-      const element = document.getElementById('typewriter');
+      const element = document.getElementById('typewriter1');
       if (index < text.length) {
-        element.textContent += text.charAt(index); // Tilføj næste bogstav
+        element.textContent += text.charAt(index); 
         index++;
-        setTimeout(typeWriter, 150); // Hastighed på 150ms per bogstav
+        setTimeout(typeWriter, 200); 
       }
     }
   
-    // Tøm teksten og start effekten
-    document.getElementById('typewriter').textContent = "";
+    document.getElementById('typewriter1').textContent = "";
+    typeWriter();
+  });
+
+  // CV TYPEWRITER EFFEKT
+  document.addEventListener("DOMContentLoaded", () => { 
+    const text = "CV"; 
+    let index = 0;
+  
+    function typeWriter() {
+      const element = document.getElementById('typewriter2');
+      if (index < text.length) {
+        element.textContent += text.charAt(index); 
+        index++;
+        setTimeout(typeWriter, 200); 
+      }
+    }
+  
+    document.getElementById('typewriter2').textContent = "";
+    typeWriter();
+  });
+
+  // FORSIDE TYPEWRITER EFFEKT
+  document.addEventListener("DOMContentLoaded", () => { 
+    const text = "Portfolio"; 
+    let index = 0;
+  
+    function typeWriter() {
+      const element = document.getElementById('typewriter3');
+      if (index < text.length) {
+        element.textContent += text.charAt(index); 
+        index++;
+        setTimeout(typeWriter, 200); 
+      }
+    }
+  
+    document.getElementById('typewriter3').textContent = "";
+    typeWriter();
+  });
+
+//   TEMA 1 TYPEWRITER EFFEKT
+document.addEventListener("DOMContentLoaded", () => { 
+    const text = "Introuge - Tema 1"; 
+    let index = 0;
+  
+    function typeWriter() {
+      const element = document.getElementById('typewriter4');
+      if (index < text.length) {
+        element.textContent += text.charAt(index); 
+        index++;
+        setTimeout(typeWriter, 200); 
+      }
+    }
+  
+    document.getElementById('typewriter4').textContent = "";
     typeWriter();
   });
